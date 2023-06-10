dfa9ae6678c7fd849d7c729b706984892d5006a6
Farjana <farjanajhn12@gmail.com>
2023-06-10 07:43:24 +0600

    added firebase configaration file

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDXfU1LHHKtfe43ak7If5qdOEjk5mXWSGg",
+  authDomain: "lingobridge-934f9.firebaseapp.com",
+  projectId: "lingobridge-934f9",
+  storageBucket: "lingobridge-934f9.appspot.com",
+  messagingSenderId: "137609491599",
+  appId: "1:137609491599:web:5e660ef2f5b1602fc42cfa"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
