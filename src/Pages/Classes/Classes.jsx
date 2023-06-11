a7d37c197b884fed8e48e74740afd8f8e78acfe5
Farjana <farjanajhn12@gmail.com>
2023-06-11 20:48:25 +0600

    dashboard added

--- src/Pages/Classes/Classes.jsx
+++ src/Pages/Classes/Classes.jsx
@@ -1,8 +1,27 @@
+import { useEffect, useState } from "react";
+import SectionTitle from "../Shared/SectionTitle";
+import Class from "./Class";
 
 const Classes = () => {
+  const [popularClasses, setPopularClasses] = useState([]);
+  useEffect(() => {
+    fetch('http://localhost:3000/classes')
+      .then(res => res.json())
+      .then(data => {
+      setPopularClasses(data)
+    })
+  },[])
   return (
     <div>
-      <h1>All the classes here</h1>
+    <SectionTitle
+        heading={"Popular Classes"}
+        subHeading={"Discover our popular language classes and embark on a journey to become fluent in a new language. Whether you're a beginner or looking to enhance your language skills, our expert instructors will guide you every step of the way. Immerse yourself in interactive lessons, practice conversations, and learn from real-life examples. With a focus on practical application and cultural understanding, our language classes offer a dynamic and engaging learning experience. Join our vibrant community of language learners and broaden your horizons today!"}
+      ></SectionTitle>
+         <div className="grid md:grid-cols-2 gap-10 mt-4">
+      {
+        popularClasses.map(popularClass=><Class key={popularClass.id} popularClass={popularClass}></Class>)
+      }
+    </div>
     </div>
   );
 };
--- src/Pages/Home/Home/Pclass.jsx
+++ src/Pages/Home/Home/Pclass.jsx
@@ -1,10 +1,9 @@
-import { useContext } from "react";
-import { AuthContext } from "../../../Providers/AuthProvider";
+
 
 
 const Pclass = ({ popularClass }) => {
-  const {user}=useContext(AuthContext)
-  const {img,available_seat,price,classe_name,name} =popularClass
+ 
+  const {img,student,price,classe_name,name} =popularClass
   
   return (
     <div>
@@ -13,15 +12,10 @@ const Pclass = ({ popularClass }) => {
   <div className="card-body">
           <h2 className="card-title">{classe_name}</h2>
           <h4>Instructor Name:{name}</h4>
-          <p>Available seat:{available_seat}</p>
+          <p>Number of student:{student}</p>
           <p>Price :${price}</p>
     <div className="card-actions justify-end">
-    <button
-      className={`bg-purple-900 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ${user ? '' : 'opacity-50 cursor-not-allowed'}`}
-      disabled={!user}
-    >
-      Select the course
-    </button>
+
     </div>
   </div>
 </div>
