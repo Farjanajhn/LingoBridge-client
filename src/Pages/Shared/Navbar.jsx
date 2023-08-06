266899acb1d19a36da260e610ce8c80ecb5116df
Farjana <farjanajhn12@gmail.com>
2023-08-06 12:38:38 +0200

    fix some problem in navber

--- src/Pages/Shared/Navbar.jsx
+++ src/Pages/Shared/Navbar.jsx
@@ -52,17 +52,17 @@ const Navbar = () => {
  
       <div className="navbar max-w-screen-xl bg-purple-900 rounded-lg">
   <div className="navbar-start">
-    <div className="dropdown  text-yellow-700">
+    <div className="dropdown text-yellow-700">
       <label tabIndex={0} className="btn btn-ghost lg:hidden">
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
       </label>
-      <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+      <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52" >
         {navOptions}
       </ul>
     </div>
     <a className="btn btn-ghost normal-case text-xl text-white">Lingo Bridge</a>
   </div>
-  <div className="navbar-center hidden lg:flex  text-white">
+  <div className="navbar-center hidden lg:flex text-white">
     <ul className="menu menu-horizontal px-1">
      {navOptions}
     </ul>
